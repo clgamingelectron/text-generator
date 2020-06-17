3d22cdc2b6293212863f19aa27c9dc6d5bbbf0fa
clgamingelectron <67078370+clgamingelectron@users.noreply.github.com>
2020-06-17 18:21:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const btn = document.querySelector("button"),
+  output = document.querySelector("p.output"),
+  outputDiv = document.querySelector("div.output");
+
+let oldRandomNum = 0,
+  oldRandomPink = 0,
+  list = [
+  "First text",
+  "Random yikes",
+  "Redditor",
+  "That's me",
+  "Hello there"
+];
+
+let pinks = [
+  "bg-pink-400",
+  "bg-pink-500",
+  "bg-pink-600",
+  "bg-pink-700",
+  "bg-pink-800"
+]
+
+btn.addEventListener("click", change);
+
+function change(){
+  changeText();
+  changePink();
+};
+
+function changeText(){
+  let newRandomNum = Math.floor(Math.random() * list.length);
+
+  if (newRandomNum !== oldRandomNum){
+    output.textContent = list[newRandomNum];
+    oldRandomNum = newRandomNum;
+  } else {
+    changeText();
+  };
+};
+
+function changePink(){
+  let newRandomPink = Math.floor(Math.random() * pinks.length);
+
+  if (newRandomPink !== oldRandomPink){
+    outputDiv.classList.remove(pinks[oldRandomPink]);
+    outputDiv.classList.add(pinks[newRandomPink]);
+    oldRandomPink = newRandomPink;
+  } else {
+    changePink();
+  };
+};
